e8cf3ad203bd5aa4b93d4241f8afbab1b393aa6e
desertsniper87 <torshobuet@gmail.com>
2020-07-30 21:01:09 +0600

    initial model

--- scripts/wasm_expl_2_wast.sh
+++ scripts/wasm_expl_2_wast.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 for wasm_file in wasm_samples/benign_samples/*.wasm; do
-  ~/wabt/bin/wasm2wat $wasm_file > wast_samples/benign_samples/"$(basename -- "$wasm_file" .wasm).wast"
+  ~/wabt/bin/wasm2wat $wasm_file > wast_samples/benign/"$(basename -- "$wasm_file" .wasm).txt"
 done
 
 for wasm_file in wasm_samples/miner_samples/*.wasm; do
-  ~/wabt/bin/wasm2wat $wasm_file > wast_samples/miner_samples/"$(basename -- "$wasm_file" .wasm).wast"
+  ~/wabt/bin/wasm2wat $wasm_file > wast_samples/miner/"$(basename -- "$wasm_file" .wasm).txt"
 done
